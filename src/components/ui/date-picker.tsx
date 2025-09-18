54e8281aba026a0e38c72d3acce9981a5348fab4
Yr3b <102194010+Yr3b@users.noreply.github.com>
2025-09-17 21:30:54 -0300

    change appointment index & add booked appointments

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -1,72 +1,106 @@
-import { useState } from "react"
-import { ChevronDownIcon } from "lucide-react"
+import { useState } from "react";
+import { ChevronDownIcon } from "lucide-react";
 
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectGroup,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectLabel,
+} from "@/components/ui/select";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-import { cn } from "@/lib/utils"
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
 
 interface DatePickerProps {
   className?: string;
   date: Date | undefined;
   setDate: (date: Date | undefined) => void;
-  time: string;
   setTime: (time: string) => void;
+  hasTimePicker: boolean;
+  availableHours: any;
 }
 
-export function DatePicker({ className, date, setDate, time, setTime }: DatePickerProps) {
-  const [open, setOpen] = useState(false)
-
+export function DatePicker({
+  className,
+  date,
+  setDate,
+  setTime,
+  hasTimePicker,
+  availableHours,
+}: DatePickerProps) {
+  const [open, setOpen] = useState(false);
+  const handleHourClick = (hour: string) => {
+    setTime(hour);
+  };
   return (
     <div className={cn("flex gap-4", className)}>
       <div className="flex flex-col gap-3">
         <Label htmlFor="date-picker" className="px-1">
           Fecha
         </Label>
-        <Popover open={open} onOpenChange={setOpen}>
-          <PopoverTrigger asChild>
-            <Button
-              variant="outline"
-              id="date-picker"
-              className="justify-between font-normal"
+        <div className="flex gap-3">
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                id="date-picker"
+                className="justify-between font-normal"
+              >
+                {date ? date.toLocaleDateString() : "Selecciona una fecha"}
+                <ChevronDownIcon />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent
+              className="w-auto overflow-hidden p-0"
+              align="start"
             >
-              {date ? date.toLocaleDateString() : "Selecciona una fecha"}
-              <ChevronDownIcon />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto overflow-hidden p-0" align="start">
-            <Calendar
-              mode="single"
-              selected={date || undefined}
-              captionLayout="dropdown"
-              onSelect={(date) => {
-                setDate(date || undefined)
-                setOpen(false)
-              }}
-            />
-          </PopoverContent>
-        </Popover>
-      </div>
-      <div className="flex flex-col gap-3">
-        <Label htmlFor="time-picker" className="px-1">
-          Hora
-        </Label>
-        <Input
-          type="time"
-          id="time-picker"
-          step="1"
-          defaultValue="00:00:00"
-          className="bg-background appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
-          value={time}
-          onChange={(e) => setTime(e.target.value)}
-        />
+              <Calendar
+                mode="single"
+                selected={date || undefined}
+                captionLayout="dropdown"
+                onSelect={(date) => {
+                  setDate(date || undefined);
+                  setOpen(false);
+                }}
+              />
+            </PopoverContent>
+          </Popover>
+          {hasTimePicker && (
+            <Select>
+              <SelectTrigger className="w-[200px]">
+                <SelectValue placeholder="Seleccione un horario" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Horarios Disponibles</SelectLabel>
+                  {availableHours?.map((item, i) => (
+                    <div key={i}>
+                      {item.hours.map((hour: string, j) => (
+                        <SelectItem
+                          onClick={() => handleHourClick(hour)}
+                          value={hour}
+                        >
+                          {hour}
+                        </SelectItem>
+                      ))}
+                    </div>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          )}
+        </div>
       </div>
     </div>
-  )
+  );
 }
