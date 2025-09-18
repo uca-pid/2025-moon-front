f4efb3fb144d4ea8edb97d9e986a168a2a7a6639
Yr3b <102194010+Yr3b@users.noreply.github.com>
2025-09-17 22:51:01 -0300

    change appointment index & add booked appointments (#5)
    * change appointment index & add booked appointments
    
    * add disabled to date picker
    
    ---------
    
    Co-authored-by: Zepoide <santi.tamarit07@gmail.com>
    Co-authored-by: Santiago <90455490+santitamaritt@users.noreply.github.com>

--- src/pages/Appointments/index.tsx
+++ src/pages/Appointments/index.tsx
@@ -2,12 +2,78 @@ import { DatePicker } from "@/components/ui/date-picker";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Container } from "@/components/Container";
 
 export const Appointments = () => {
   const [service, setService] = useState<string>("");
   const [date, setDate] = useState<Date | undefined>(undefined);
   const [time, setTime] = useState<string>("");
+
+  const availableHours = [
+    {
+      workshop: "example",
+      hours: ["08:00", "10:00"],
+    },
+  ];
+
+  const Appointments = [
+    {
+      id: 1,
+      date: "2025-09-20",
+      hour: "10:00",
+      workshop: "example",
+      servicio: "nose",
+    },
+    {
+      id: 2,
+      date: "2025-09-21",
+      hour: "14:00",
+      workshop: "example",
+      servicio: "nose",
+    },
+    {
+      id: 3,
+      date: "2025-09-22",
+      hour: "16:30",
+      workshop: "example",
+      servicio: "nose",
+    },
+    {
+      id: 4,
+      date: "2025-09-23",
+      hour: "18:00",
+      workshop: "example",
+      servicio: "nose",
+    },
+    {
+      id: 1,
+      date: "2025-09-20",
+      hour: "10:00",
+      workshop: "example",
+      servicio: "nose",
+    },
+    {
+      id: 2,
+      date: "2025-09-21",
+      hour: "14:00",
+      workshop: "example",
+      servicio: "nose",
+    },
+    {
+      id: 3,
+      date: "2025-09-22",
+      hour: "16:30",
+      workshop: "example",
+      servicio: "nose",
+    },
+  ];
 
   const services = [
     {
@@ -25,19 +91,24 @@ export const Appointments = () => {
     {
       name: "Revisión",
       price: null,
-    }
-  ]
+    },
+  ];
 
   const handleDisabled = () => {
     return !service || !date || !time || service === "";
-  }
+  };
 
   return (
     <Container>
       <div className="mx-auto flex max-w-6xl items-center justify-start px-4 py-3 w-full">
         <div className="flex flex-col gap-8 w-full">
-          <DatePicker date={date} setDate={setDate} time={time} setTime={setTime} />
-
+          <DatePicker
+            hasTimePicker={true}
+            date={date}
+            setDate={setDate}
+            setTime={setTime}
+            availableHours={availableHours}
+          />
           <div className="flex flex-col gap-3 w-[50%] cursor-pointer">
             <Label htmlFor="service" className="px-1">
               Servicio
@@ -59,11 +130,37 @@ export const Appointments = () => {
               ))}
             </select>
           </div>
-
           <div>
-            <Button variant="outline" className="px-6 py-5 text-base" disabled={handleDisabled()}>
+            <Button
+              variant="outline"
+              className="px-6 py-5 text-base"
+              disabled={handleDisabled()}
+            >
               Confirmar
             </Button>
+          </div>
+          <div className="flex flex-col gap-3 w-[50%]">
+            <Label htmlFor="appointments" className="px-1">
+              Turnos agendados
+            </Label>
+            <ScrollArea className="h-[400px] w-[800px] rounded-md border">
+              <div>
+                {Appointments.map((app) => {
+                  return (
+                    <Card className="flex  ">
+                      <CardHeader>
+                        <CardTitle>Turno #{app.id}</CardTitle>
+                        <CardDescription>
+                          Tienes un turno agendado en el taller {app.workshop}{" "}
+                          el dia {app.date} a las {app.hour} para realizar el
+                          servicio {app.servicio}.
+                        </CardDescription>
+                      </CardHeader>
+                    </Card>
+                  );
+                })}
+              </div>
+            </ScrollArea>
           </div>
         </div>
       </div>
