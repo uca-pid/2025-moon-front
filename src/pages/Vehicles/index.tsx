9d972ee06a208b9ad66b0fa5cff9d6493b27db7b
Yr3b <102194010+Yr3b@users.noreply.github.com>
2025-09-30 23:20:59 -0300

    Add Update & Delete

--- src/pages/Vehicles/index.tsx
+++ src/pages/Vehicles/index.tsx
@@ -1,5 +1,3 @@
-// hacer BM
-
 import { Container } from "@/components/Container";
 import {
   AlertDialog,
@@ -40,8 +38,17 @@ import { Label } from "@/components/ui/label";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { CirclePlus, Pencil } from "lucide-react";
 import { useEffect, useState } from "react";
-import { createVehicle, getVehiclesOfUser } from "@/services/vehicles";
-import type { CreateVehicle, Vehicle } from "@/types/vehicles.types";
+import {
+  createVehicle,
+  deleteVehicle,
+  getVehiclesOfUser,
+  updateVehicle,
+} from "@/services/vehicles";
+import type {
+  CreateVehicle,
+  UpdateVehicle,
+  Vehicle,
+} from "@/types/vehicles.types";
 import {
   Accordion,
   AccordionContent,
@@ -51,14 +58,20 @@ import {
 import { toast } from "sonner";
 
 export const Vehicles = () => {
-  const [licensePlate, setlicensePlate] = useState("");
+  const [licensePlate, setLicensePlate] = useState("");
   const [model, setModel] = useState("");
   const [year, setYear] = useState(0);
   const [km, setKm] = useState(0);
   const [status, setStatus] = useState<"idle" | "success" | "error">("idle");
   const [open, setOpen] = useState(false);
+  const [openChange, setOpenChange] = useState(false);
   const [refreshVehiclesTick, setRefreshVehiclesTick] = useState<number>(0);
   const [Vehicles, setVehicles] = useState<Vehicle[]>([]);
+  const [licensePlateChange, setLicensePlateChange] = useState("");
+  const [modelChange, setModelChange] = useState("");
+  const [yearChange, setYearChange] = useState(2025);
+  const [kmChange, setKmChange] = useState(0);
+  const [id, setId] = useState(0);
 
   useEffect(() => {
     const fetchVehicles = async () => {
@@ -72,8 +85,57 @@ export const Vehicles = () => {
     }
   }, [refreshVehiclesTick]);
 
+  const setVehicleToBeChange = (vehicle: Vehicle) => {
+    setLicensePlateChange(vehicle.licensePlate);
+    setModelChange(vehicle.model);
+    setYearChange(vehicle.year);
+    setKmChange(vehicle.km);
+    setId(vehicle.id);
+  };
+
+  const handleDeleteVehicle = async (id: number) => {
+    try {
+      console.log(id);
+      await deleteVehicle(id);
+      setRefreshVehiclesTick((prev) => prev + 1);
+      setStatus("success");
+      toast.success("Vehiculo eliminado correctamente.");
+      setOpen(false);
+    } catch (error) {
+      setStatus("error");
+      toast.error(
+        "Hubo un error eliminando su vehiculo por favor intente de nuevo."
+      );
+    }
+  };
+
+  const handleUpdateVehicle = async (
+    e: React.FormEvent<HTMLFormElement>,
+    id: number
+  ) => {
+    e.preventDefault();
+    const vehicle: UpdateVehicle = {
+      licensePlate: licensePlateChange,
+      model: modelChange,
+      year: yearChange,
+      km: kmChange,
+    };
+    try {
+      console.log(id);
+      await updateVehicle(id, vehicle);
+      setRefreshVehiclesTick((prev) => prev + 1);
+      setStatus("success");
+      toast.success("Vehiculo editado correctamente.");
+      setOpen(false);
+    } catch (error) {
+      setStatus("error");
+      toast.error(
+        "Hubo un error editando los datos de su vehiculo por favor intente de nuevo."
+      );
+    }
+  };
+
   const handleAddVehicle = async (e: React.FormEvent<HTMLFormElement>) => {
-    console.log("entre");
     e.preventDefault();
     const vehicle: CreateVehicle = {
       licensePlate: licensePlate,
@@ -88,6 +150,10 @@ export const Vehicles = () => {
       setStatus("success");
       toast.success("Vehiculo agregado correctamente.");
       setOpen(false);
+      setKm(0);
+      setLicensePlate("");
+      setModel("");
+      setYear(2025);
     } catch (error) {
       console.log(error);
       setStatus("error");
@@ -130,12 +196,13 @@ export const Vehicles = () => {
                           </AccordionItem>
                         </Accordion>
                         <div className="flex flex-row gap-2">
-                          <Sheet>
+                          <Sheet open={openChange} onOpenChange={setOpenChange}>
                             <SheetTrigger asChild className="text-foreground">
                               <Button
                                 variant="secondary"
                                 size="icon"
                                 className="size-8"
+                                onClick={() => setVehicleToBeChange(vehicle)}
                               >
                                 <Pencil />
                               </Button>
@@ -151,52 +218,71 @@ export const Vehicles = () => {
                                   no olvides guardar los cambios.
                                 </SheetDescription>
                               </SheetHeader>
-                              <div className="grid auto-rows-min gap-4 px-4">
-                                <div className="grid gap-3">
-                                  <Label htmlFor="licensePlate">Patente</Label>
-                                  <Input
-                                    id="licensePlate"
-                                    name="licensePlate"
-                                    defaultValue={vehicle.licensePlate}
-                                  />
+                              <form
+                                onSubmit={(e) => handleUpdateVehicle(e, id)}
+                                className="flex flex-col gap-4"
+                              >
+                                <div className="grid auto-rows-min gap-4 px-4">
+                                  <div className="grid gap-3">
+                                    <Label htmlFor="licensePlate">
+                                      Patente
+                                    </Label>
+                                    <Input
+                                      id="licensePlate"
+                                      name="licensePlate"
+                                      value={licensePlateChange}
+                                      onChange={(e) =>
+                                        setLicensePlateChange(e.target.value)
+                                      }
+                                    />
+                                  </div>
                                 </div>
-                              </div>
-                              <div className="grid auto-rows-min gap-4 px-4">
-                                <div className="grid gap-3">
-                                  <Label htmlFor="model">Modelo</Label>
-                                  <Input
-                                    id="model"
-                                    name="model"
-                                    defaultValue={vehicle.model}
-                                  />
+                                <div className="grid auto-rows-min gap-4 px-4">
+                                  <div className="grid gap-3">
+                                    <Label htmlFor="model">Modelo</Label>
+                                    <Input
+                                      id="model"
+                                      name="model"
+                                      value={modelChange}
+                                      onChange={(e) =>
+                                        setModelChange(e.target.value)
+                                      }
+                                    />
+                                  </div>
                                 </div>
-                              </div>
-                              <div className="grid auto-rows-min gap-4 px-4">
-                                <div className="grid gap-3">
-                                  <Label htmlFor="year">Año</Label>
-                                  <Input
-                                    id="year"
-                                    name="year"
-                                    defaultValue={vehicle.year}
-                                  />
+                                <div className="grid auto-rows-min gap-4 px-4">
+                                  <div className="grid gap-3">
+                                    <Label htmlFor="year">Año</Label>
+                                    <Input
+                                      id="year"
+                                      name="year"
+                                      value={yearChange}
+                                      onChange={(e) =>
+                                        setYearChange(Number(e.target.value))
+                                      }
+                                    />
+                                  </div>
                                 </div>
-                              </div>
-                              <div className="grid auto-rows-min gap-4 px-4">
-                                <div className="grid gap-3">
-                                  <Label htmlFor="km">Kilometraje</Label>
-                                  <Input
-                                    id="km"
-                                    name="km"
-                                    defaultValue={vehicle.km}
-                                  />
+                                <div className="grid auto-rows-min gap-4 px-4">
+                                  <div className="grid gap-3">
+                                    <Label htmlFor="km">Kilometraje</Label>
+                                    <Input
+                                      id="km"
+                                      name="km"
+                                      value={kmChange}
+                                      onChange={(e) =>
+                                        setKmChange(Number(e.target.value))
+                                      }
+                                    />
+                                  </div>
                                 </div>
-                              </div>
-                              <SheetFooter>
-                                <Button type="submit">Guardar cambios</Button>
-                                <SheetClose asChild>
-                                  <Button variant="outline">Salir</Button>
-                                </SheetClose>
-                              </SheetFooter>
+                                <SheetFooter>
+                                  <Button type="submit">Guardar cambios</Button>
+                                  <SheetClose asChild>
+                                    <Button variant="outline">Salir</Button>
+                                  </SheetClose>
+                                </SheetFooter>
+                              </form>
                             </SheetContent>
                           </Sheet>
 
@@ -205,7 +291,12 @@ export const Vehicles = () => {
                               asChild
                               className="text-foreground"
                             >
-                              <Button variant="destructive">Eliminar</Button>
+                              <Button
+                                variant="destructive"
+                                onClick={() => setId(vehicle.id)}
+                              >
+                                Eliminar
+                              </Button>
                             </AlertDialogTrigger>
                             <AlertDialogContent className="text-foreground">
                               <AlertDialogHeader>
@@ -223,7 +314,11 @@ export const Vehicles = () => {
                               </AlertDialogHeader>
                               <AlertDialogFooter>
                                 <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                                <AlertDialogAction>Eliminar</AlertDialogAction>
+                                <AlertDialogAction
+                                  onClick={() => handleDeleteVehicle(id)}
+                                >
+                                  Eliminar
+                                </AlertDialogAction>
                               </AlertDialogFooter>
                             </AlertDialogContent>
                           </AlertDialog>
@@ -261,7 +356,7 @@ export const Vehicles = () => {
                       id="licensePlate"
                       name="licensePlate"
                       value={licensePlate}
-                      onChange={(e) => setlicensePlate(e.target.value)}
+                      onChange={(e) => setLicensePlate(e.target.value)}
                       required
                     />
                   </div>
--- src/types/vehicles.types.ts
+++ src/types/vehicles.types.ts
@@ -1,3 +1,10 @@
+export interface UpdateVehicle {
+  licensePlate: string;
+  model: string;
+  year: number;
+  km: number;
+}
+
 export interface CreateVehicle {
   licensePlate: string;
   model: string;
@@ -6,6 +13,7 @@ export interface CreateVehicle {
 }
 
 export interface Vehicle {
+  id: number;
   licensePlate: string;
   model: string;
   year: number;
